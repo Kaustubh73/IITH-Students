bac162458297a01c85ef225aaf133acf8c4f24cf
Kaustubh <kaustubhg73@gmail.com>
2022-06-15 07:17:29 +0530

    changed README.md and yayy.py

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # IITH-Students
 Making a web app for the students, by the students
+to the students too as this project will go to them only
--- yayy.py
+++ yayy.py
@@ -1,1 +1,3 @@
 print("Oo making a new website or something of sorts cool. \n But with friens tho even cooler :D")
+
+print("Another line, just for funsies")
